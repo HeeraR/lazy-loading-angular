545bbb0ccee303ba9c0ea63624556d38e142db76
HeeraR <heeramadhurathat2000@gmail.com>
2024-07-09 09:38:41 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { FormsComponent } from './registration/form/form.component';
+import { LoginModule } from './login/login.module';
 
 const routes: Routes = [
+  {path: '/forms', 'loadChildren: () => import('./login/login.module.ts')
+    .then(m => m.LazyLoadingModule)},
   { path: '', redirectTo: '/forms', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: FormsComponent },
